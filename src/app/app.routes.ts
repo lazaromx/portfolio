e2f28eee9117557f1fd40121cadd9f5e3887ccd7
Lázaro Marques <lazaro.marquesx@gmail.com>
2025-07-25 12:18:40 -0300

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'projects', //
+    loadComponent: () =>
+      import('./pages/projects/projects.component').then(
+        (m) => m.ProjectsComponent
+      ),
+  },
+];
