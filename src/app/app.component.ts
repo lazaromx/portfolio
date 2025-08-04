949f876d88eb82d6834880cf0ee9f8b76c25aa7d
Lázaro Marques <lazaro.marquesx@gmail.com>
2025-08-04 13:48:09 -0300

    Feat: Add Component About

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {HomeComponent} from './components/home/home.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProjectsComponent, HomeComponent],
+  imports: [HeaderComponent, ProjectsComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
