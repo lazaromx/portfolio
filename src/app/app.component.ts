c14caf00898d0665a6424c5175826ae504376b74
Lázaro Marques <lazaro.marquesx@gmail.com>
2025-08-06 14:01:17 -0300

    Feat: add skills logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 import {HomeComponent} from './components/home/home.component'
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ProjectsComponent, HomeComponent],
+  imports: [HeaderComponent, ProjectsComponent, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { ProjectsComponent } from './pages/projects/projects.component';
-
 export const routes: Routes = [
   // {
   //   path: 'projects', //
